ba6aab42599404c76061cebd5df5358a01958eb7
Davor Cubranic <davor@phas.ubc.ca>
2017-03-03 16:52:21 -0800

    Refactor the original archive copy model into alpenhorn.acquisition

---
+++ alpenhorn/archive.py
@@ -0,0 +1,67 @@
+import peewee as pw
+from db import base_model, EnumField
+from alpenhorn.storage import *
+from alpenhorn.acquisition import *
+
+
+class ArchiveFileCopy(base_model):
+    """Information about a file.
+
+    Attributes
+    ----------
+    file : foreign key
+        Reference to the file of which this is a copy.
+    node : foreign key
+        The node on which this copy lives (or should live).
+    has_file : string
+        Is the node on the file?
+        - 'Y': yes, the node has the file.
+        - 'N': no, the node does not have the file.
+        - 'M': maybe: we've tried to copy/erase it, but haven't yet verified.
+        - 'X': the file is there, but has been verified to be corrupted.
+    wants_file : enum
+        Does the node want the file?
+        - 'Y': yes, keep the file around
+        - 'M': maybe, can delete if we need space
+        - 'N': no, attempt to delete
+        In all cases we try to keep at least two copies of the file around.
+    """
+    file = pw.ForeignKeyField(ArchiveFile, related_name='copies')
+    node = pw.ForeignKeyField(StorageNode, related_name='copies')
+    has_file = EnumField(['N', 'Y', 'M', 'X'], default='N')
+    wants_file = EnumField(['Y', 'M', 'N'], default='Y')
+
+
+class ArchiveFileCopyRequest(base_model):
+    """Requests for file copies.
+
+    Attributes
+    ----------
+    file : foreign key
+        Reference to the file to be copied.
+    group_to : foreign key
+        The storage group to which the file should be copied.
+    node_from : foreign key
+        The node from which the file should be copied.
+    nice : integer
+        For nicing the copy/rsync process if resource management is needed.
+    completed : bool
+        Set to true when the copy has succeeded.
+    cancelled : bool
+        Set to true if the copy is no longer wanted.
+    n_requests : integer
+        The number of previous requests that have been made for this copy.
+    timestamp : datetime
+        The time the most recent request was made.
+    """
+    file = pw.ForeignKeyField(ArchiveFile, related_name='requests')
+    group_to = pw.ForeignKeyField(StorageGroup, related_name='requests_to')
+    node_from = pw.ForeignKeyField(StorageNode, related_name='requests_from')
+    nice = pw.IntegerField()
+    completed = pw.BooleanField()
+    cancelled = pw.BooleanField(default=False)
+    n_requests = pw.IntegerField()
+    timestamp = pw.DateTimeField()
+
+    class Meta:
+        primary_key = pw.CompositeKey('file', 'group_to', 'node_from')
---
+++ tests/test_archive_model.py
@@ -0,0 +1,98 @@
+"""
+test_archive_model
+------------------
+
+Tests for `alpenhorn.archive` module.
+"""
+
+import pytest
+import yaml
+from os import path
+
+import alpenhorn.db as db
+from alpenhorn.archive import *
+import alpenhorn.storage as storage
+import alpenhorn.acquisition as acquisition
+
+import test_storage_model as ts
+import test_acquisition_model as ta
+
+tests_path = path.abspath(path.dirname(__file__))
+
+@pytest.fixture
+def fixtures():
+    """Initializes an in-memory Sqlite database with data in tests/fixtures"""
+    db.connect()
+
+
+    fs = ts.fixtures(False).next()
+    fa = ta.fixtures(False).next()
+
+    db.database_proxy.create_tables([ArchiveFileCopy, ArchiveFileCopyRequest ])
+
+    # Check we're starting from a clean slate
+    assert ArchiveFileCopy.select().count() == 0
+    assert ArchiveFileCopyRequest.select().count() == 0
+    assert StorageNode.select().count() != 0
+
+    with open(path.join(tests_path, 'fixtures/archive.yml')) as f:
+        fixtures = yaml.load(f)
+
+    # fixup foreign keys for the file copies
+    for copy in fixtures['file_copies']:
+        copy['file']= fa['files'][copy['file']]
+        copy['node']= fs['nodes'][copy['node']]
+
+    # bulk load the file copies
+    ArchiveFileCopy.insert_many(fixtures['file_copies']).execute()
+    file_copies = dict(ArchiveFileCopy.select(ArchiveFileCopy.file, ArchiveFileCopy.id).tuples())
+
+    # fixup foreign keys for the copy requests
+    for req in fixtures['copy_requests']:
+        req['file']= fa['files'][req['file']]
+        req['node_from']= fs['nodes'][req['node_from']]
+        req['group_to']= fs['groups'][req['group_to']]
+
+    # bulk load the file copies
+    ArchiveFileCopyRequest.insert_many(fixtures['copy_requests']).execute()
+    copy_requests = list(ArchiveFileCopyRequest.select(ArchiveFileCopyRequest.file,
+                                                       ArchiveFileCopyRequest.node_from,
+                                                       ArchiveFileCopyRequest.group_to).tuples())
+
+    yield {
+        'file_copies': file_copies,
+        'copy_requests': copy_requests
+    }
+
+    # cleanup
+    db.database_proxy.close()
+
+
+def test_schema(fixtures):
+    assert set(db.database_proxy.get_tables() )== {
+        u'storagegroup', u'storagenode',
+        u'acqtype', u'archiveinst', u'archiveacq',
+        u'filetype', u'archivefile',
+        u'archivefilecopyrequest', u'archivefilecopy'
+    }
+
+
+def test_model(fixtures):
+    copies = set(ArchiveFileCopy
+                 .select(ArchiveFile.name, StorageNode.name)
+                 .join(ArchiveFile)
+                 .switch(ArchiveFileCopy)
+                 .join(StorageNode).tuples())
+    assert copies == { ('fred', 'x'), ('sheila', 'x')}
+
+    reqs = set(ArchiveFileCopyRequest
+               .select(ArchiveFile.name, StorageNode.name, StorageGroup.name)
+               .join(ArchiveFile)
+               .switch(ArchiveFileCopyRequest)
+               .join(StorageNode)
+               .switch(ArchiveFileCopyRequest)
+               .join(StorageGroup)
+               .tuples())
+    assert reqs == { ('jim', 'x', 'bar') }
+
+    assert ArchiveFileCopy.select().join(ArchiveFile).where(ArchiveFile.name == 'sheila').get().wants_file == 'M'
