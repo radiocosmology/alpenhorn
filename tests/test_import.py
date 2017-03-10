b17d720ae3a57fa7053f53d27cd25469d65fcae8
Davor Cubranic <davor@phas.ubc.ca>
2017-03-10 14:51:49 -0800

    Move creation of test files for import to the unit test fixture

--- tests/test_import.py
+++ tests/test_import.py
@@ -38,7 +38,16 @@ def fixtures(tmpdir):
     # TODO: we need to either handle some built-in types; or maybe move to the YAML fixtures
     ac.FileType.create(name='log')
 
-    return {'root': p}
+    with open(os.path.join(tests_path, 'fixtures/files.yml')) as f:
+        fixtures = yaml.load(f)
+
+    for dir_name, files in fixtures.iteritems():
+        d = p.mkdir(dir_name)
+        for file_name, file_data in files.iteritems():
+            f = d.join(file_name)
+            f.write(file_data['contents'])
+
+    return {'root': p, 'files': fixtures}
 
 
 def test_schema(fixtures):
@@ -52,51 +61,43 @@ def test_schema(fixtures):
     assert fixtures['root'].basename == 'ROOT'
     assert st.StorageNode.get(st.StorageNode.name == 'x').root == fixtures['root']
 
+    tmpdir = fixtures['root']
+    assert len(tmpdir.listdir()) == 1
+    acq_dir = tmpdir.join(fixtures['files'].keys()[0])
+    assert len(acq_dir.listdir()) == 2
+
 
 def test_import(fixtures):
     tmpdir = fixtures['root']
 
-    # TODO: these should move to fixtures
-    acq_dir = tmpdir.mkdir('12345678T000000Z_inst_zab')
-    f = acq_dir.join("hello.txt")
-    f.write('Hello world!')
-    assert f.read() == 'Hello world!'
-    g = acq_dir.join("ch_master.log")
-    g.write('I''m the master of the world!')
-    assert g.read() == 'I''m the master of the world!'
-    assert len(tmpdir.listdir()) == 1
-    assert len(acq_dir.listdir()) == 2
-
-    print "hello.txt: %s"
-    for arf in ac.ArchiveFile.select().where(ac.ArchiveFile.name == f.basename):
-        print "  - id: %s" % arf.id
+    acq_dir = tmpdir.join('12345678T000000Z_inst_zab')
 
     node = st.StorageNode.get(st.StorageNode.name == 'x')
 
     # import for hello.txt should be ignored while creating the acquisition
-    auto_import.import_file(node, node.root, acq_dir.basename, f.basename)
+    auto_import.import_file(node, node.root, acq_dir.basename, 'hello.txt')
     assert ac.ArchiveInst.get(ac.ArchiveInst.name == 'inst') is not None
     assert ac.AcqType.get(ac.AcqType.name == 'zab') is not None
 
-    acq = ac.ArchiveAcq.get(ac.ArchiveAcq.name == '12345678T000000Z_inst_zab')
+    acq = ac.ArchiveAcq.get(ac.ArchiveAcq.name == acq_dir.basename)
     assert acq is not None
-    assert acq.name == '12345678T000000Z_inst_zab'
+    assert acq.name == acq_dir.basename
     assert acq.inst.name == 'inst'
     assert acq.type.name == 'zab'
 
     assert (ac.ArchiveFile
             .select()
-            .where(ac.ArchiveFile.name == f.basename)
+            .where(ac.ArchiveFile.acq == acq)
             .count()) == 0
 
     # now import 'ch_master.log', which should succeed
-    auto_import.import_file(node, node.root, acq_dir.basename, g.basename)
+    auto_import.import_file(node, node.root, acq_dir.basename, 'ch_master.log')
     file = ac.ArchiveFile.get(ac.ArchiveFile.name == 'ch_master.log')
     assert file is not None
     assert file.acq == acq
     assert file.type.name == 'log'
-    assert file.size_b == 27
-    assert file.md5sum == '1fb43de52e12f866a8a444e9f786e1c0'
+    assert file.size_b == len(fixtures['files'][acq_dir.basename][file.name]['contents'])
+    assert file.md5sum == fixtures['files'][acq_dir.basename][file.name]['md5']
 
     file_copy = ar.ArchiveFileCopy.get(ar.ArchiveFileCopy.file == file,
                                        ar.ArchiveFileCopy.node == node)
