232663a9c047f3984ee7e33f61b4d65f9181d43a
Davor Cubranic <davor@phas.ubc.ca>
2020-05-06 08:38:04 -0700

    feat: Add an 'acq syncable' command to list files that can be synced
    This command is an "inspection-only" version of `sync`: executing it is
    essentially equivalent to running `sync --acq=ACQ NODE TARGET`, viewing the list
    of files it prints, and responding "N" so that it does not actually add the copy
    request.

--- alpenhorn/client/acq.py
+++ alpenhorn/client/acq.py
@@ -97,3 +97,66 @@ def files(acquisition):
         print(tabulate.tabulate(data, headers=['Name', 'Size']))
     else:
         print("No registered archive files.")
+
+@cli.command()
+@click.argument('acquisition')
+@click.argument('source_node')
+@click.argument('destination_group')
+def syncable(acquisition, source_node, destination_group):
+    """List all files that are in the ACQUISITION.
+    """
+    config_connect()
+
+    import tabulate
+
+    try:
+        acq = ac.ArchiveAcq.get(name=acquisition)
+    except pw.DoesNotExist:
+        print("No such acquisition:", acquisition)
+
+    try:
+        src = st.StorageNode.get(name=source_node)
+    except pw.DoesNotExist:
+        print("No such storage node:", source_node)
+        sys.exit(1)
+
+    try:
+        dest = st.StorageGroup.get(name=destination_group)
+    except pw.DoesNotExist:
+        print("No such storage group:", destination_group)
+        sys.exit(1)
+
+    # First get the nodes at the destination...
+    nodes_at_dest = st.StorageNode.select().where(st.StorageNode.group == dest)
+
+    # Then use this to get a list of all files at the destination...
+    files_at_dest = ac.ArchiveFile.select().join(ar.ArchiveFileCopy).where(
+        ac.ArchiveFile.acq == acq,
+        ar.ArchiveFileCopy.node << nodes_at_dest,
+        ar.ArchiveFileCopy.has_file == 'Y',
+    )
+
+    # Then combine to get all file(copies) that are available at the source but
+    # not at the destination...
+    query = (
+        ac.ArchiveFile.select(
+            ac.ArchiveFile.name,
+            ac.ArchiveFile.size_b,
+        )
+        .where(ac.ArchiveFile.acq == acq)
+        .join(ar.ArchiveFileCopy)
+        .where(
+            ar.ArchiveFileCopy.node == src,
+            ar.ArchiveFileCopy.has_file == 'Y',
+            ~(ar.ArchiveFile.id << files_at_dest),
+        )
+    )
+
+    data = query.tuples()
+
+    if data:
+        print(tabulate.tabulate(data, headers=['Name', 'Size']))
+    else:
+        print("No files to copy from node '", source_node,
+              "' to group '", destination_group, "'.", sep="")
+
