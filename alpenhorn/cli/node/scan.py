450c068a104ef76465fd3bf5ebca99f38442f7f9
ketiltrout <dvw@phas.ubc.ca>
2024-11-18 13:02:10 -0800

    fix(CLI): Re-implement "node scan"
    Completely re-implements "node scan" to use the new
    ArchiveFileImportRequest table from #235.
    
    It's a very simple command, now.

---
+++ alpenhorn/cli/node/scan.py
@@ -0,0 +1,62 @@
+"""alpenhorn node scan command"""
+
+import click
+import pathlib
+import peewee as pw
+
+from ...db import ArchiveFileImportRequest, StorageNode, database_proxy
+from ..cli import echo
+
+
+@click.command()
+@click.argument("name", metavar="NODE")
+@click.argument("path", metavar="PATH", required=False, default=".")
+@click.option(
+    "--register-new",
+    "-n",
+    help="Register unknown files instead of ignoring them.",
+    is_flag=True,
+)
+def scan(name, path, register_new):
+    """Scan a node for files to import.
+
+    This command will submit a request to ask the daemon managing NODE to
+    scan PATH for new files to add to the Node.  if PATH is absolute, it
+    must start with the Node root path.  Otherwise, it's assumed PATH is
+    relative to Node root.  If not given, PATH defaults to "." (i.e. the scan
+    is performed on the entire Node).
+
+    By default, only files already registered in the data index will be imported
+    to NODE.  Unknown files will be skipped.  This can be changed, however, by
+    using the --register-new flag, which tells the daemon to create new records
+    for new files which are determined to be importable.
+    """
+
+    with database_proxy.atomic():
+        try:
+            node = StorageNode.get(name=name)
+        except pw.DoesNotExist:
+            raise click.ClickException("no such node: " + name)
+
+        # Strip node root, if absolute
+        path = pathlib.PurePath(path)
+
+        if path.is_absolute():
+            if node.root is None:
+                raise click.ClickException(
+                    f'PATH can\'t be absolute: Node "{node.name}" has no root.'
+                )
+
+            try:
+                path = path.relative_to(node.root)
+            except ValueError as e:
+                raise click.ClickException(
+                    f'absolute path "{path}" outside node root: ' + node.root
+                ) from e
+
+        # submit the request
+        ArchiveFileImportRequest.create(
+            node=node, path=str(path), recurse=True, register=register_new
+        )
+
+        echo(f'Added request for scan of "{path}" on Node "{node.name}".')
---
+++ tests/cli/node/test_scan.py
@@ -0,0 +1,130 @@
+"""Test CLI: alpenhorn node scan"""
+
+from alpenhorn.db import StorageGroup, StorageNode, ArchiveFileImportRequest, utcnow
+
+
+def test_no_node(clidb, cli):
+    """Test a missing NODE name"""
+
+    group = StorageGroup.create(name="Group")
+    StorageNode.create(name="Node", group=group)
+
+    cli(2, ["node", "scan"])
+
+
+def test_bad_node(clidb, cli):
+    """Test a bad node name"""
+
+    group = StorageGroup.create(name="Group")
+    StorageNode.create(name="Node", group=group)
+
+    cli(1, ["node", "scan", "MISSING"])
+
+
+def test_default(clidb, cli):
+    """Test the default invocation."""
+
+    group = StorageGroup.create(name="Group")
+    node = StorageNode.create(name="Node", group=group, root="/root")
+
+    cli(0, ["node", "scan", "Node"])
+
+    # Request created
+    afir = ArchiveFileImportRequest.get(id=1)
+
+    assert afir.node == node
+    assert afir.path == "."
+    assert afir.recurse == 1
+    assert afir.register == 0
+
+
+def test_relpath(clidb, cli):
+    """Test a relative PATH."""
+
+    group = StorageGroup.create(name="Group")
+    node = StorageNode.create(name="Node", group=group, root="/root")
+
+    cli(0, ["node", "scan", "Node", "relative/path"])
+
+    # Request created
+    afir = ArchiveFileImportRequest.get(id=1)
+
+    assert afir.node == node
+    assert afir.path == "relative/path"
+    assert afir.recurse == 1
+    assert afir.register == 0
+
+
+def test_relpath(clidb, cli):
+    """Test a relative PATH."""
+
+    group = StorageGroup.create(name="Group")
+    node = StorageNode.create(name="Node", group=group, root="/root")
+
+    cli(0, ["node", "scan", "Node", "relative/path"])
+
+    # Request created
+    afir = ArchiveFileImportRequest.get(id=1)
+
+    assert afir.node == node
+    assert afir.path == "relative/path"
+    assert afir.recurse == 1
+    assert afir.register == 0
+
+
+def test_abspath(clidb, cli):
+    """Test an absolute PATH."""
+
+    group = StorageGroup.create(name="Group")
+    node = StorageNode.create(name="Node", group=group, root="/root")
+
+    cli(0, ["node", "scan", "Node", "/root/absolute/path"])
+
+    # Request created
+    afir = ArchiveFileImportRequest.get(id=1)
+
+    assert afir.node == node
+    assert afir.path == "absolute/path"
+    assert afir.recurse == 1
+    assert afir.register == 0
+
+
+def test_bad_abspath(clidb, cli):
+    """Test an invalid absolute PATH."""
+
+    group = StorageGroup.create(name="Group")
+    node = StorageNode.create(name="Node", group=group, root="/root")
+
+    cli(1, ["node", "scan", "Node", "/absolute/path"])
+
+    # No request created
+    assert ArchiveFileImportRequest.select().count() == 0
+
+
+def test_abspath_no_root(clidb, cli):
+    """Test an absolute PATH but NODE has no root."""
+
+    group = StorageGroup.create(name="Group")
+    node = StorageNode.create(name="Node", group=group, root=None)
+
+    cli(1, ["node", "scan", "Node", "/absolute/path"])
+
+    # No request created
+    assert ArchiveFileImportRequest.select().count() == 0
+
+
+def test_new(clidb, cli):
+    """Test --register-new."""
+
+    group = StorageGroup.create(name="Group")
+    node = StorageNode.create(name="Node", group=group, root=None)
+
+    cli(0, ["node", "scan", "Node", "--register-new"])
+
+    # Request created
+    afir = ArchiveFileImportRequest.get(id=1)
+
+    assert afir.node == node
+    assert afir.path == "."
+    assert afir.recurse == 1
+    assert afir.register == 1
